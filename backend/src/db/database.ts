49758c6cb41ff60c4383e65b5a8775b12d02eb26
Yegon <yegonkimutai.99@gmail.com>
2026-09-10 20:38:04 +0300

    Create database for inquiries

---
+++ backend/src/db/database.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { Pool } from "pg";
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.on("error", (error) => {
+  console.error("Unexpected database error:", error);
+});
---
+++ backend/src/db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE inquiries (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+    email VARCHAR(255) NOT NULL
+    subject VARCHAR(200) NOT NULL
+    message TEXT NOT NULL
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
